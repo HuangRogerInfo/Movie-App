e460a84c881b1af5924a43dab12fa99c0448aebf
YounesAEO <y.aboueleomoum09@gmail.com>
2022-12-05 21:30:41 +0100

    implment add to collection and connect collection page to backend

--- src/app/film.service.ts
+++ src/app/film.service.ts
@@ -2,18 +2,20 @@ import { Injectable } from "@angular/core";
 import { Film } from "./film";
 import { FILMS } from "./mock-film-list";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { AuthService } from "./auth.service";
 
 const FILMS_ENDPOINT = "http://localhost:3000/films";
 const USERS_ENDPOINT = "http://localhost:3000/users";
 
 @Injectable()
 export class FilmService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private auth: AuthService) {}
   getFilmList(): Film[] {
     return FILMS;
   }
 
-  getLatestFilms(): any {
+  getLatestFilms(): Observable<any> {
     return this.http.get(`${FILMS_ENDPOINT}/latest`);
     // return FILMS;
   }
@@ -27,7 +29,24 @@ export class FilmService {
     return ["sci-fi", "action", "animation", "drame", "musical"];
   }
 
-  getFavFilms(userId: string): any {
+  getFavFilms(userId: string): Observable<any> {
     return this.http.get(`${USERS_ENDPOINT}/${userId}/list`);
+  }
+
+  isFilmAdded(filmId: number) {
+    // console.log(this.auth.connectedUser.data.favFilms.find((film: number) => film === filmId));
+    return this.auth.connectedUser?.data?.favFilms?.find(
+      (film: number) => film === filmId
+    );
+  }
+
+  deleteFilmFromCollection(filmId: number, userId: string): Observable<any> {
+    return this.http.delete(`${USERS_ENDPOINT}/${userId}/list/${filmId}`);
+  }
+
+  addFilmToCollection(filmId: number, userId: string) {
+    return this.http.put(`${USERS_ENDPOINT}/${userId}/list`, {
+      filmId,
+    });
   }
 }
--- src/app/list-movie/list-movie.component.ts
+++ src/app/list-movie/list-movie.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { AuthService } from "../auth.service";
 import { Film } from "../film";
 import { FilmService } from "../film.service";
 import { OpinionsService } from "../opinions.service";
@@ -15,6 +16,7 @@ export class ListMovieComponent implements OnInit {
   constructor(
     private router: Router,
     private filmService: FilmService,
+    public authService: AuthService,
     public opinionService: OpinionsService
   ) {}
 
@@ -31,7 +33,28 @@ export class ListMovieComponent implements OnInit {
     this.router.navigate(["/films", film._id]);
   }
 
-  addToCollection(film: Film) {
-    this.opinionService.addEmptyOpinion(film);
+  addToCollection(filmId: number) {
+    // this.opinionService.addEmptyOpinion(film);
+    const userId = this.authService.connectedUser?.data?.userId;
+    this.filmService.addFilmToCollection(filmId, userId).subscribe((user) => {
+      //@ts-ignore
+      this.authService.connectedUser.data.favFilms = user.favFilms;
+    });
+  }
+
+  isFilmAdded(filmId: number) {
+    return this.filmService.isFilmAdded(filmId);
+  }
+
+  deleteFromCollection(filmId: number) {
+    const userId = this.authService.connectedUser?.data?.userId;
+    if (userId) {
+      this.filmService.deleteFilmFromCollection(filmId, userId).subscribe(
+        (user) => {
+          this.authService.connectedUser.data.favFilms = user.favFilms;
+        },
+        (error: any) => console.error(error)
+      );
+    }
   }
 }
