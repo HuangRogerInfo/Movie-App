a8b139833769c5ba9c4a68e23360800481ac063c
Abdesamad Meziane <abdesamad.meziane.pro@hotmail.com>
2022-12-08 12:13:25 +0100

    modification profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../auth.service";
+import { Film } from "../film";
+import { FilmService } from "../film.service";
+
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"],
 })
 export class ProfileComponent implements OnInit {
-  constructor(public authService: AuthService) {}
-
-  ngOnInit(): void {
-    
+  filmList: Film[];
+  noteList: any[];
+  constructor(
+    private router: Router,
+    public authService: AuthService,
+    public filmService: FilmService
+  ) {
+    this.filmList = [];
   }
 
+  ngOnInit(): void {
+    if (this.authService.connectedUser) {
+      this.filmService
+        .getFavFilms(this.authService.connectedUser?.data?.userId)
+        .subscribe((favFilms: Film[]) => {
+          this.filmList = favFilms;
+        });
+    } else {
+      this.router.navigate(["/home"]);
+    }
+  }
+  goToFilm(film: Film) {
+    this.router.navigate(["/films", film._id]);
+  }
 }
